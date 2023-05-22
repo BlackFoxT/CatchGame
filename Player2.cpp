a4e5cd136a78f31ea28fa0236c9c9fd6700c9e5a
Zubair1683 <134060453+Zubair1683@users.noreply.github.com>
2023-05-22 23:27:22 +0300

    Picking the secondplayer's texture and frame into the itself class
    I picked secondplayer's texture and frame into the itself class.

--- Player2.cpp
+++ Player2.cpp
@@ -1,35 +1,38 @@
 #include "raylib.h"
 #include "Player2.hpp"
 
-
 Player2::Player2() {
 	x = 300;
 	y = 400;
 	speed = 3;
+	Texture2D p2movetexture;
+	Rectangle p2moveframe;
 }
-void Player2::up() {
-	//up button 
-
+void Player2::up(Texture2D texture, Rectangle frame) {
+	p2movetexture = texture;
+	p2moveframe = frame;
 	y -= speed;
 	if (y < 0)
 		y = 0;
 }
-void Player2::down() {
-	//down button
+void Player2::down(Texture2D texture, Rectangle frame) {
+	p2movetexture = texture;
+	p2moveframe = frame;
 	y += speed;
 	if (y > 950)
 		y = 950;
 }
-void Player2::right() {
-	//right button
+void Player2::right(Texture2D texture, Rectangle frame) {
+	p2movetexture = texture;
+	p2moveframe = frame;
 	x += speed;
 	if (x > 950)
 		x = 950;
 }
-void Player2::left() {
-	//left button
+void Player2::left(Texture2D texture, Rectangle frame) {
+	p2movetexture = texture;
+	p2moveframe = frame;
 	x -= speed;
 	if (x < 0)
 		x = 0;
-
 }
