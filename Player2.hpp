184c1d0e537c5f6c0e3906002c2728f0c736e6c4
Zubair1683 <134060453+Zubair1683@users.noreply.github.com>
2023-05-22 23:27:47 +0300

    Update Player2.hpp

--- Player2.hpp
+++ Player2.hpp
@@ -7,9 +7,11 @@ public:
 	int point = 0;
 	float x, y;
 	float speed;
-	void up();
-	void down();
-	void right();
-	void left();
+	Texture2D p2movetexture;
+	Rectangle p2moveframe;
+	void up(Texture2D texture, Rectangle frame);
+	void down(Texture2D texture, Rectangle frame);
+	void right(Texture2D texture, Rectangle frame);
+	void left(Texture2D texture, Rectangle frame);
 
 };
