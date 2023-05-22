38860129180bd4ad50e0e87fde77c7dd995bb031
Zubair1683 <134060453+Zubair1683@users.noreply.github.com>
2023-05-22 23:25:28 +0300

    Picking the firstplayer's texture and frame into the itself class
    I picked the firstplayer's texture and frame into the itself class.

--- Player1.cpp
+++ Player1.cpp
@@ -5,30 +5,34 @@ Player1::Player1() {
 	x = 625;
 	y = 500;
 	speed = 3;
+	Texture2D p1movetexture;
+	Rectangle p1moveframe;
 }
-void Player1::up() {
-	//up button 
-	
+void Player1::up(Texture2D texture, Rectangle frame) {
+	p1movetexture = texture;
+	p1moveframe = frame;
 	y -= speed;
 	if (y < 0)
 		y = 0;
 }
-void Player1::down() {
-	//down button
+void Player1::down(Texture2D texture, Rectangle frame) {
+	p1movetexture = texture;
+	p1moveframe = frame;
 	y += speed;
 	if (y > 950)
 		y = 950;
 }
-void Player1::right() {
-	//right button
+void Player1::right(Texture2D texture, Rectangle frame) {
+	p1movetexture = texture;
+	p1moveframe = frame;
 	x += speed;
 	if (x > 950)
 		x = 950;
 }
-void Player1::left() {
-	//left button
+void Player1::left(Texture2D texture, Rectangle frame) {
+	p1movetexture = texture;
+	p1moveframe = frame;
 	x -= speed;
 	if (x < 0)
 		x = 0;
-
 }
