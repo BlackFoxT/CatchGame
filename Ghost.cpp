8fdb6293678102721c993b2aef40687549c66869
Zubair1683 <134060453+Zubair1683@users.noreply.github.com>
2023-05-22 23:21:58 +0300

    Picking the ghosts texture and frame into the itself.
    I picked the ghosts texture and frame into the itself.

--- Ghost.cpp
+++ Ghost.cpp
@@ -1,36 +1,40 @@
 #include "raylib.h"
 #include "Ghost.hpp"
 
-
 Ghost::Ghost(int z, int t) {
 	x = z;
 	y = t;
 	speed = 2;
+	Texture2D ghostmovetexture;
+	Rectangle ghostmoveframe;
 }
 Ghost::~Ghost() {
 }
-void Ghost::up() {
-	//up button 
-
+void Ghost::up(Texture2D texture, Rectangle frame) {
+	ghostmovetexture= texture;
+	ghostmoveframe = frame;
 	y -= speed;
 	if (y < 0)
 		y = 0;
 
 }
-void Ghost::down() {
-	//down button
+void Ghost::down(Texture2D texture, Rectangle frame) {
+	ghostmovetexture = texture;
+	ghostmoveframe = frame;
 	y += speed;
 	if (y > 950)
 		y = 950;
 }
-void Ghost::right() {
-	//right button
+void Ghost::right(Texture2D texture, Rectangle frame) {
+	ghostmovetexture = texture;
+	ghostmoveframe = frame;
 	x += speed;
 	if (x > 950)
 		x = 950;
 }
-void Ghost::left() {
-	//left button
+void Ghost::left(Texture2D texture, Rectangle frame) {
+	ghostmovetexture= texture;
+	ghostmoveframe = frame;
 	x -= speed;
 	if (x < 0)
 		x = 0;
