f5fdde00b8ed3c1b9ecca501b4aa3a964b9ca50c
alisen1907 <134067263+alisen1907@users.noreply.github.com>
2023-05-27 14:12:52 +0300

    Adding new movement functions about ghost

--- Ghost.hpp
+++ Ghost.hpp
@@ -13,7 +13,10 @@ public:
 	void down(Texture2D texture, Rectangle frame);
 	void right(Texture2D texture, Rectangle frame);
 	void left(Texture2D texture, Rectangle frame);
-
+	void upright(Texture2D texture, Rectangle frame);
+	void upleft(Texture2D texture, Rectangle frame);
+	void downright(Texture2D texture, Rectangle frame);
+	void downleft(Texture2D texture, Rectangle frame);
 
 };
 
