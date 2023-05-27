7407e8f69625489fb40f8602e5d2cbb9430bdcdf
alisen1907 <134067263+alisen1907@users.noreply.github.com>
2023-05-27 14:14:00 +0300

    Adding new movement functions about ghost

--- Ghost.cpp
+++ Ghost.cpp
@@ -11,7 +11,8 @@ Ghost::Ghost(int z, int t) {
 Ghost::~Ghost() {
 }
 void Ghost::up(Texture2D texture, Rectangle frame) {
-	ghostmovetexture= texture;
+	//up button 
+	ghostmovetexture = texture;
 	ghostmoveframe = frame;
 	y -= speed;
 	if (y < 0)
@@ -19,6 +20,7 @@ void Ghost::up(Texture2D texture, Rectangle frame) {
 
 }
 void Ghost::down(Texture2D texture, Rectangle frame) {
+	//down button
 	ghostmovetexture = texture;
 	ghostmoveframe = frame;
 	y += speed;
@@ -26,6 +28,7 @@ void Ghost::down(Texture2D texture, Rectangle frame) {
 		y = 950;
 }
 void Ghost::right(Texture2D texture, Rectangle frame) {
+	//right button
 	ghostmovetexture = texture;
 	ghostmoveframe = frame;
 	x += speed;
@@ -33,10 +36,51 @@ void Ghost::right(Texture2D texture, Rectangle frame) {
 		x = 950;
 }
 void Ghost::left(Texture2D texture, Rectangle frame) {
-	ghostmovetexture= texture;
+	//left button
+	ghostmovetexture = texture;
 	ghostmoveframe = frame;
 	x -= speed;
 	if (x < 0)
 		x = 0;
 
+}
+void Ghost::upright(Texture2D texture, Rectangle frame) {
+	ghostmovetexture = texture;
+	ghostmoveframe = frame;
+	x += speed;
+	y -= speed;
+	if (x > 950)
+		x = 950;
+	if (y < 0)
+		y = 0;
+}
+void Ghost::upleft(Texture2D texture, Rectangle frame) {
+	ghostmovetexture = texture;
+	ghostmoveframe = frame;
+	x -= speed;
+	y -= speed;
+	if (x < 0)
+		x = 0;
+	if (y < 0)
+		y = 0;
+}
+void Ghost::downright(Texture2D texture, Rectangle frame) {
+	ghostmovetexture = texture;
+	ghostmoveframe = frame;
+	x += speed;
+	y += speed;
+	if (x > 950)
+		x = 950;
+	if (y > 950)
+		y = 950;
+}
+void Ghost::downleft(Texture2D texture, Rectangle frame) {
+	ghostmovetexture = texture;
+	ghostmoveframe = frame;
+	x -= speed;
+	y += speed;
+	if (x < 0)
+		x = 0;
+	if (y > 950)
+		y = 950;
 }
