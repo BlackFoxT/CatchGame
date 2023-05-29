434518448ca843cb741917172cf828227f2495a9
Zubair1683 <134060453+Zubair1683@users.noreply.github.com>
2023-05-29 16:33:30 +0300

    Making one of the functions parameter to default parameter.
    I added a parameter to each functions and made it as default parameter.

--- Ghost.cpp
+++ Ghost.cpp
@@ -10,75 +10,73 @@ Ghost::Ghost(int z, int t) {
 }
 Ghost::~Ghost() {
 }
-void Ghost::up(Texture2D texture, Rectangle frame) {
+void Ghost::up(Texture2D texture, Rectangle frame, int a) {
 	//up button 
 	ghostmovetexture = texture;
 	ghostmoveframe = frame;
-	y -= speed;
+	y -= a + speed;
 	if (y < 0)
 		y = 0;
-
 }
-void Ghost::down(Texture2D texture, Rectangle frame) {
+void Ghost::down(Texture2D texture, Rectangle frame, int a) {
 	//down button
 	ghostmovetexture = texture;
 	ghostmoveframe = frame;
-	y += speed;
+	y += a + speed;
 	if (y > 950)
 		y = 950;
 }
-void Ghost::right(Texture2D texture, Rectangle frame) {
+void Ghost::right(Texture2D texture, Rectangle frame, int a) {
 	//right button
 	ghostmovetexture = texture;
 	ghostmoveframe = frame;
-	x += speed;
+	x += a + speed;
 	if (x > 950)
 		x = 950;
 }
-void Ghost::left(Texture2D texture, Rectangle frame) {
+void Ghost::left(Texture2D texture, Rectangle frame, int a) {
 	//left button
 	ghostmovetexture = texture;
 	ghostmoveframe = frame;
-	x -= speed;
+	x -= a + speed;
 	if (x < 0)
 		x = 0;
-
 }
-void Ghost::upright(Texture2D texture, Rectangle frame) {
+void Ghost::upright(Texture2D texture, Rectangle frame, int a) {
 	ghostmovetexture = texture;
 	ghostmoveframe = frame;
-	x += speed;
-	y -= speed;
+	x += a + speed;
+	y -= a + speed;
 	if (x > 950)
 		x = 950;
 	if (y < 0)
 		y = 0;
 }
-void Ghost::upleft(Texture2D texture, Rectangle frame) {
+void Ghost::upleft(Texture2D texture, Rectangle frame, int a) {
 	ghostmovetexture = texture;
 	ghostmoveframe = frame;
-	x -= speed;
-	y -= speed;
+	x -= a + speed;
+	y -= a + speed;
 	if (x < 0)
 		x = 0;
 	if (y < 0)
 		y = 0;
 }
-void Ghost::downright(Texture2D texture, Rectangle frame) {
+void Ghost::downright(Texture2D texture, Rectangle frame, int a) {
 	ghostmovetexture = texture;
 	ghostmoveframe = frame;
-	x += speed;
-	y += speed;
+	x += a + speed;
+	y += a + speed;
 	if (x > 950)
 		x = 950;
 	if (y > 950)
 		y = 950;
 }
-void Ghost::downleft(Texture2D texture, Rectangle frame) {
+void Ghost::downleft(Texture2D texture, Rectangle frame, int a) {
 	ghostmovetexture = texture;
 	ghostmoveframe = frame;
-	x -= speed;
-	y += speed;
+	x -= a + speed;
+	y += a + speed;
 	if (x < 0)
 		x = 0;
 	if (y > 950)
