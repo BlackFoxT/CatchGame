3895bc2d7d225cfa6d2e5dad03f727a32584aeba
Hidayet <125586147+BlackFoxT@users.noreply.github.com>
2023-05-29 16:26:25 +0300

    Designing Player1.hpp for Movement class

--- Player1.hpp
+++ Player1.hpp
@@ -1,6 +1,7 @@
 #pragma once
+#include "Movement.hpp"
 
-class Player1
+class Player1:public Movement	
 {
 public:
 	Player1();
@@ -9,12 +10,12 @@ public:
 	float speed;
 	Texture2D p1movetexture;
 	Rectangle p1moveframe;
-	void up(Texture2D texture, Rectangle frame);
-	void down(Texture2D texture, Rectangle frame);
-	void right(Texture2D texture, Rectangle frame);
-	void left(Texture2D texture, Rectangle frame);
-	void upright(Texture2D texture, Rectangle frame);
-	void upleft(Texture2D texture, Rectangle frame);
-	void downright(Texture2D texture, Rectangle frame);
-	void downleft(Texture2D texture, Rectangle frame);
+	void up(Texture2D texture, Rectangle frame, int a = 0);
+	void down(Texture2D texture, Rectangle frame, int a = 0);
+	void right(Texture2D texture, Rectangle frame, int a = 0);
+	void left(Texture2D texture, Rectangle frame, int a = 0);
+	void upright(Texture2D texture, Rectangle frame, int a = 0);
+	void upleft(Texture2D texture, Rectangle frame, int a = 0);
+	void downright(Texture2D texture, Rectangle frame, int a = 0);
+	void downleft(Texture2D texture, Rectangle frame, int a = 0);
 };
