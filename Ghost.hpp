fd6dabfaa58708c93924567a9b5b5cd455ed336e
Zubair1683 <134060453+Zubair1683@users.noreply.github.com>
2023-05-29 16:35:18 +0300

    Extending this class to Movement class
    This class extends to Movement class and has a default parameter at each function.

--- Ghost.hpp
+++ Ghost.hpp
@@ -1,22 +1,22 @@
 #pragma once
+#include "Movement.hpp"
 
-class Ghost
+class Ghost : public Movement
 {
-public:
+public: 
 	Ghost(int z, int t);
 	~Ghost();
 	float x, y;
 	float speed;
 	Texture2D ghostmovetexture;
 	Rectangle ghostmoveframe;
-	void up(Texture2D texture, Rectangle frame);
-	void down(Texture2D texture, Rectangle frame);
-	void right(Texture2D texture, Rectangle frame);
-	void left(Texture2D texture, Rectangle frame);
-	void upright(Texture2D texture, Rectangle frame);
-	void upleft(Texture2D texture, Rectangle frame);
-	void downright(Texture2D texture, Rectangle frame);
-	void downleft(Texture2D texture, Rectangle frame);
+	void up(Texture2D texture, Rectangle frame, int a = 0);
+	void down(Texture2D texture, Rectangle frame, int a = 0);
+	void right(Texture2D texture, Rectangle frame, int a = 0);
+	void left(Texture2D texture, Rectangle frame, int a = 0);
+	void upright(Texture2D texture, Rectangle frame, int a = 0);
+	void upleft(Texture2D texture, Rectangle frame, int a = 0);
+	void downright(Texture2D texture, Rectangle frame, int a = 0);
+	void downleft(Texture2D texture, Rectangle frame, int a = 0);
 
 };
-
