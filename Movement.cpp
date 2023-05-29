cae5d95daea71ddeab8062d1d0e4863b9d7fb647
Hidayet <125586147+BlackFoxT@users.noreply.github.com>
2023-05-29 16:24:21 +0300

    Adding Movement class
    I add this class for ghosts and players

---
+++ Movement.cpp
@@ -0,0 +1,19 @@
+#include "raylib.h"
+#include "Movement.hpp"
+
+	void up(Texture2D texture, Rectangle frame, int a){
+	}
+	void down(Texture2D texture, Rectangle frame, int a) {
+	}
+	void right(Texture2D texture, Rectangle frame, int a) {
+	}
+	void left(Texture2D texture, Rectangle frame, int a) {
+	}
+	void upright(Texture2D texture, Rectangle frame, int a) {
+	}
+	void upleft(Texture2D texture, Rectangle frame, int a) {
+	}
+	void downright(Texture2D texture, Rectangle frame, int a) {
+	}
+	void downleft(Texture2D texture, Rectangle frame, int a){
+	}
---
+++ Movement.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+class Movement
+{
+	public:
+
+	void up(Texture2D texture, Rectangle frame, int a = 0);
+	void down(Texture2D texture, Rectangle frame, int a = 0);
+	void right(Texture2D texture, Rectangle frame, int a = 0);
+	void left(Texture2D texture, Rectangle frame, int a = 0);
+	void upright(Texture2D texture, Rectangle frame, int a = 0);
+	void upleft(Texture2D texture, Rectangle frame, int a = 0);
+	void downright(Texture2D texture, Rectangle frame, int a = 0);
+	void downleft(Texture2D texture, Rectangle frame, int a = 0);
+};
