56a051f007c2a3c7f12ef3958d9a095cdcef990d
Zubair1683 <134060453+Zubair1683@users.noreply.github.com>
2023-05-30 14:31:39 +0300

    Update Movement.cpp

--- Movement.cpp
+++ Movement.cpp
@@ -1,15 +1,19 @@
-#pragma once
+#include "raylib.h"
+#include "Movement.hpp"
 
-class Movement
-{
-public:
-
-	void up(Texture2D texture, Rectangle frame);
-	void down(Texture2D texture, Rectangle frame);
-	void right(Texture2D texture, Rectangle frame);
-	void left(Texture2D texture, Rectangle frame);
-	void upright(Texture2D texture, Rectangle frame);
-	void upleft(Texture2D texture, Rectangle frame);
-	void downright(Texture2D texture, Rectangle frame);
-	void downleft(Texture2D texture, Rectangle frame);
-};
+void up(Texture2D texture, Rectangle frame) {
+}
+void down(Texture2D texture, Rectangle frame) {
+}
+void right(Texture2D texture, Rectangle frame) {
+}
+void left(Texture2D texture, Rectangle frame) {
+}
+void upright(Texture2D texture, Rectangle frame) {
+}
+void upleft(Texture2D texture, Rectangle frame) {
+}
+void downright(Texture2D texture, Rectangle frame) {
+}
+void downleft(Texture2D texture, Rectangle frame) {
+}
