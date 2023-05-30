ea66941ab5b60611de4fa8f8c8805f84b766435d
Zubair1683 <134060453+Zubair1683@users.noreply.github.com>
2023-05-30 14:29:57 +0300

    Update Ghost.cpp

--- Ghost.cpp
+++ Ghost.cpp
@@ -10,73 +10,69 @@ Ghost::Ghost(int z, int t) {
 }
 Ghost::~Ghost() {
 }
-void Ghost::up(Texture2D texture, Rectangle frame, int a) {
-	//up button 
+void Ghost::up(Texture2D texture, Rectangle frame) {
 	ghostmovetexture = texture;
 	ghostmoveframe = frame;
-	y -= a + speed;
+	y -= speed;
 	if (y < 0)
 		y = 0;
 }
-void Ghost::down(Texture2D texture, Rectangle frame, int a) {
-	//down button
+void Ghost::down(Texture2D texture, Rectangle frame) {
 	ghostmovetexture = texture;
 	ghostmoveframe = frame;
-	y += a + speed;
+	y += speed;
 	if (y > 950)
 		y = 950;
 }
-void Ghost::right(Texture2D texture, Rectangle frame, int a) {
-	//right button
+void Ghost::right(Texture2D texture, Rectangle frame) {
 	ghostmovetexture = texture;
 	ghostmoveframe = frame;
-	x += a + speed;
+	x += speed;
 	if (x > 950)
 		x = 950;
 }
-void Ghost::left(Texture2D texture, Rectangle frame, int a) {
-	//left button
+void Ghost::left(Texture2D texture, Rectangle frame) {
 	ghostmovetexture = texture;
 	ghostmoveframe = frame;
-	x -= a + speed;
+	x -= speed;
 	if (x < 0)
 		x = 0;
 }
-void Ghost::upright(Texture2D texture, Rectangle frame, int a) {
+void Ghost::upright(Texture2D texture, Rectangle frame) {
 	ghostmovetexture = texture;
 	ghostmoveframe = frame;
-	x += a + speed;
-	y -= a + speed;
+	x += speed;
+	y -= speed;
 	if (x > 950)
 		x = 950;
 	if (y < 0)
 		y = 0;
 }
-void Ghost::upleft(Texture2D texture, Rectangle frame, int a) {
+void Ghost::upleft(Texture2D texture, Rectangle frame) {
 	ghostmovetexture = texture;
 	ghostmoveframe = frame;
-	x -= a + speed;
-	y -= a + speed;
+	x -= speed;
+	y -= speed;
 	if (x < 0)
 		x = 0;
 	if (y < 0)
 		y = 0;
 }
-void Ghost::downright(Texture2D texture, Rectangle frame, int a) {
+void Ghost::downright(Texture2D texture, Rectangle frame) {
 	ghostmovetexture = texture;
 	ghostmoveframe = frame;
-	x += a + speed;
-	y += a + speed;
+	x += speed;
+	y += speed;
 	if (x > 950)
 		x = 950;
 	if (y > 950)
 		y = 950;
 }
-void Ghost::downleft(Texture2D texture, Rectangle frame, int a) {
+void Ghost::downleft(Texture2D texture, Rectangle frame) {
 	ghostmovetexture = texture;
 	ghostmoveframe = frame;
-	x -= a + speed;
-	y += a + speed;
+	x -= speed;
+	y += speed;
 	if (x < 0)
 		x = 0;
 	if (y > 950)
