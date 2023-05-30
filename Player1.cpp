43f3a578688c7edd1b748863b448b69503ac866b
Zubair1683 <134060453+Zubair1683@users.noreply.github.com>
2023-05-30 14:33:30 +0300

    Update Player1.cpp

--- Player1.cpp
+++ Player1.cpp
@@ -1,5 +1,6 @@
 #include "raylib.h"
 #include "Player1.hpp"
+#include "Movement.hpp"
 
 Player1::Player1() {
 	x = 625;
@@ -7,73 +8,70 @@ Player1::Player1() {
 	speed = 3;
 	Texture2D p1movetexture;
 	Rectangle p1moveframe;
-}/*
-std::void void operator + (int t) {
-
-}*/
-void Player1::up(Texture2D texture, Rectangle frame, int a) {
+}
+void Player1::up(Texture2D texture, Rectangle frame) {
 	p1movetexture = texture;
 	p1moveframe = frame;
-	y -= a + speed;
+	y -= speed;
 	if (y < 0)
 		y = 0;
 }
-void Player1::down(Texture2D texture, Rectangle frame, int a) {
+void Player1::down(Texture2D texture, Rectangle frame) {
 	p1movetexture = texture;
 	p1moveframe = frame;
 	y += speed;
 	if (y > 950)
 		y = 950;
 }
-void Player1::right(Texture2D texture, Rectangle frame, int a) {
+void Player1::right(Texture2D texture, Rectangle frame) {
 	p1movetexture = texture;
 	p1moveframe = frame;
-	x += a + speed;
+	x += speed;
 	if (x > 950)
 		x = 950;
 }
-void Player1::left(Texture2D texture, Rectangle frame, int a) {
+void Player1::left(Texture2D texture, Rectangle frame) {
 	p1movetexture = texture;
 	p1moveframe = frame;
-	x -= a + speed;
+	x -= speed;
 	if (x < 0)
 		x = 0;
 }
-void Player1::upright(Texture2D texture, Rectangle frame, int a) {
+void Player1::upright(Texture2D texture, Rectangle frame) {
 	p1movetexture = texture;
 	p1moveframe = frame;
-	x += a + speed;
-	y -= a + speed;
+	x += speed;
+	y -= speed;
 	if (x > 950)
 		x = 950;
 	if (y < 0)
 		y = 0;
 }
-void Player1::upleft(Texture2D texture, Rectangle frame, int a) {
+void Player1::upleft(Texture2D texture, Rectangle frame) {
 	p1movetexture = texture;
 	p1moveframe = frame;
-	x -= a + speed;
-	y -= a + speed;
+	x -= speed;
+	y -= speed;
 	if (x < 0)
 		x = 0;
 	if (y < 0)
 		y = 0;
 }
-void Player1::downright(Texture2D texture, Rectangle frame, int a) {
+void Player1::downright(Texture2D texture, Rectangle frame) {
 	p1movetexture = texture;
 	p1moveframe = frame;
-	x += a + speed;
-	y += a + speed;
+	x += speed;
+	y += speed;
 	if (x > 950)
 		x = 950;
 	if (y > 950)
 		y = 950;
 }
-void Player1::downleft(Texture2D texture, Rectangle frame, int a) {
+void Player1::downleft(Texture2D texture, Rectangle frame) {
 	p1movetexture = texture;
 	p1moveframe = frame;
-	x -= a + speed;
-	y += a + speed;
+	x -= speed;
+	y += speed;
 	if (x < 0)
 		x = 0;
 	if (y > 950)
